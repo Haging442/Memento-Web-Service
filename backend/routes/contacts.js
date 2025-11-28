7c380804eda7664176b41c4f92795c8e9560ca64
KIM HAGYEONG <167047680+Haging442@users.noreply.github.com>
2025-11-28 14:30:10 +0900

    Delete backend directory

--- backend/routes/contacts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../auth');
-const ctrl = require('../controllers/contacts');
-
-router.get('/', auth, ctrl.getContacts);
-router.post('/', auth, ctrl.createContact);
-
-module.exports = router;
--- backend/routes/deathreports.js
+++
@@ -1,9 +0,0 @@
-// routes/deathReports.js
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/deathreports');
-
-// 로그인 필요 없이 신고 가능
-router.post('/', ctrl.createReport);
-
-module.exports = router;
